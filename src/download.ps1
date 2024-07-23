9727f9f25c5c9f104da3a6ee7f6d25c0a8b2e141
Hendrik du Toit <hendrik@brightedge.co.za>
2024-07-24 01:39:05 +0200

    Installation procedure

--- src/download.ps1
+++ src/download.ps1
@@ -12,10 +12,10 @@ $tag = (Invoke-WebRequest "https://api.github.com/repos/BrightEdgeeServices/venv
 # Download the install.ps1 file to the temporary directory
 $installScriptPath = Join-Path -Path $tempDir.FullName -ChildPath "install.ps1"
 Invoke-WebRequest -Uri "https://github.com/BrightEdgeeServices/venvit/releases/download/$tag/install.ps1" -OutFile $installScriptPath
-Write-Host "Downloaded install.ps1 to $installScriptPath"
+Write-Host "Downloaded install.ps1 to $tempDir"
 
 # Execute the install.ps1 script
-& $installScriptPath -release $tag
+& $installScriptPath -release $tag -installScriptPath $installScriptPath
 
 Remove-Item -Path $tempDir.FullName -Recurse -Force
 Write-Host "Temporary directory removed."
