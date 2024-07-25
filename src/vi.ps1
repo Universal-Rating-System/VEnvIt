d921067afcef4fbb10472168ccfba42b71e50358
Hendrik du Toit <hendrik@brightedge.co.za>
2024-07-25 02:06:06 +0200

    Replace RTE_ENVIRONMENT and SCRIPTS_DIR

--- src/vi.ps1
+++ src/vi.ps1
@@ -4,7 +4,7 @@ function DisplayEnvironmentVariables {
     Write-Host "VENV_ENVIRONMENT:      $env:VENV_ENVIRONMENT"
     Write-Host "PROJECTS_BASE_DIR:     $env:PROJECTS_BASE_DIR"
     Write-Host "PROJECT_DIR:           $env:PROJECT_DIR"
-    Write-Host "SCRIPTS_DIR:           $env:SCRIPTS_DIR"
+    Write-Host "VENVIT_DIR:           $env:VENVIT_DIR"
     Write-Host "SECRETS_DIR:           $env:SECRETS_DIR"
     Write-Host "VENV_BASE_DIR:         $env:VENV_BASE_DIR"
     Write-Host "VENV_PYTHON_BASE_DIR:  $env:VENV_PYTHON_BASE_DIR"
@@ -31,16 +31,14 @@ function InitVirtualEnvironment {
         return
     }
 
-    Write-Host "Initialize the  $_project_name virtual environment"
-
     # Check for required environment variables and display help if they're missing
-    if (-not $env:VENV_ENVIRONMENT -or -not $env:SCRIPTS_DIR -or -not $env:SECRETS_DIR -or -not $env:PROJECTS_BASE_DIR -or -not $env:VENV_BASE_DIR) {
+    if (-not $env:VENV_ENVIRONMENT -or -not $env:VENVIT_DIR -or -not $env:SECRETS_DIR -or -not $env:PROJECTS_BASE_DIR -or -not $env:VENV_BASE_DIR) {
         ShowEnvVarHelp
         return
     }
     # Set local variables from environment variables
     $_project_base_dir = $env:PROJECTS_BASE_DIR
-    $_scripts_dir = $env:SCRIPTS_DIR
+    $_venvit_dir = $env:VENVIT_DIR
     $_secrets_dir = $env:SECRETS_DIR
     $_venv_base_dir = $env:VENV_BASE_DIR
     $_venv_dir = "$_venv_base_dir\${_project_name}_env"
@@ -52,7 +50,7 @@ function InitVirtualEnvironment {
         "No virtual environment is active."
     }
     & "$_venv_dir\Scripts\activate.ps1"
-    & "${_scripts_dir}\venv_${_project_name}_setup_mandatory.ps1" $_project_name
+    & "${_venvit_dir}\venv_${_project_name}_setup_mandatory.ps1" $_project_name
     $_project_dir = $env:PROJECT_DIR
 
     if ($env:VENV_ENVIRONMENT -eq "loc_dev") {
@@ -71,7 +69,7 @@ function InitVirtualEnvironment {
         Set-Location -Path $_project_base_dir
     }
 
-    & "${_scripts_dir}\venv_${_project_name}_setup_custom.ps1" $_project_name
+    & "${_venvit_dir}\venv_${_project_name}_setup_custom.ps1" $_project_name
 }
 
 function ShowEnvVarHelp {
@@ -80,7 +78,7 @@ function ShowEnvVarHelp {
     $_env_vars = @(
         @("VENV_ENVIRONMENT", $env:VENV_ENVIRONMENT),
         @("PROJECTS_BASE_DIR", "$env:PROJECTS_BASE_DIR"),
-        @("SCRIPTS_DIR", "$env:SCRIPTS_DIR"),
+        @("VENVIT_DIR", "$env:VENVIT_DIR"),
         @("SECRETS_DIR", "$env:SECRETS_DIR"),
         @("VENV_BASE_DIR", "$env:VENV_BASE_DIR")
     )
@@ -118,7 +116,7 @@ venv_${_project_name}_setup_custom .ps1 and venv_${_project_name}_setup_mandator
     1. VENV_ENVIRONMENT:   Sets the development environment. Possible values: loc_dev, github_dev, prod, etc.
     2. PROJECTS_BASE_DIR: The directory for all projects (e.g., d:\Dropbox\Projects).
     3. SECRETS_DIR:       Directory for storing secrets (e.g., g:\Google Drive\Secrets).
-    4. SCRIPTS_DIR:       Directory where this script resides.
+    4. VENVIT_DIR:       Directory where this script resides.
     5. VENV_BASE_DIR:     Directory for virtual environments (e.g., c:\venv).
 "@ | Write-Host
 Write-Host $separator -ForegroundColor Cyan
@@ -140,7 +138,7 @@ function ShowEnvVarHelp {
     $_env_vars = @(
         @("VENV_ENVIRONMENT", $env:VENV_ENVIRONMENT),
         @("PROJECTS_BASE_DIR", "$env:PROJECTS_BASE_DIR"),
-        @("SCRIPTS_DIR", "$env:SCRIPTS_DIR"),
+        @("VENVIT_DIR", "$env:VENVIT_DIR"),
         @("SECRETS_DIR", "$env:SECRETS_DIR"),
         @("VENV_BASE_DIR", "$env:VENV_BASE_DIR")
     )
@@ -161,7 +159,8 @@ function ShowEnvVarHelp {
 Write-Host ''
 Write-Host ''
 $dateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-Write-Host "=[ START $dateTime ]==================================================" -ForegroundColor Blue
+Write-Host "=[ START $dateTime ]=======================================[ vi.ps1 ]=" -ForegroundColor Blue
+Write-Host "Initialize the $args[0] virtual environment" -ForegroundColor Blue
 InitVirtualEnvironment -_project_name $args[0]
 DisplayEnvironmentVariables
 Write-Host '-[ END ]------------------------------------------------------------------------' -ForegroundColor Cyan
