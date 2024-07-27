68512d680afc52da22fe97a954a6fd713e00f9ec
Hendrik du Toit <hendrik@brightedge.co.za>
2024-07-28 01:40:23 +0200

    Update vr.ps1

--- src/complete_install.ps1
+++ src/complete_install.ps1
@@ -45,7 +45,7 @@ function Remove-EnvVarIfExists {
 Write-Host ''
 Write-Host ''
 $dateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-Write-Host "=[ START $dateTime ]==================================[ install.ps1 ]=" -ForegroundColor Blue
+Write-Host "=[ START $dateTime ]=========================[ complete_install.ps1 ]=" -ForegroundColor Blue
 Write-Host "Install venvit" -ForegroundColor Blue
 
 Write-Host $separator -ForegroundColor Cyan
@@ -53,7 +53,7 @@ Write-Host $separator -ForegroundColor Cyan
 $url = "https://github.com/BrightEdgeeServices/venvit/releases/download/$release/installation_files.zip"
 $zipFilePath = Join-Path -Path $installScriptDir -ChildPath "installation_files.zip"
 
-Check for administrative privileges
+# Check for administrative privileges
 if (-not (Test-Admin)) {
     Write-Host "This script needs to be run as an administrator. Please run it in an elevated PowerShell session." -ForegroundColor Red
     exit
