164b3f1a0073317175e84ada05605ab101da3145
Hendrik du Toit <hendrik@brightedge.co.za>
2024-10-21 15:09:26 +0200

    Implement New-Directories

--- tests/Install.Tests.ps1
+++ tests/Install.Tests.ps1
@@ -18,19 +18,24 @@
         }
 
         It "Should Invoke-ConcludeInstall" {
+            Write-Host "*** Checkpoint 1 ***"
             Mock Set-ExecutionPolicy {}
+            Write-Host "*** Checkpoint 2 ***"
             Mock Invoke-WebRequest {
                 return @"
                     [{"tag_name": "$MockTag"}]
 "@
             } -ParameterFilter { $Uri -eq "https://api.github.com/repos/BrightEdgeeServices/venvit/releases" }
+            Write-Host "*** Checkpoint 3 ***"
             Mock Invoke-WebRequest {
                 Copy-Item -Path $PSScriptRoot\..\src\Install-Conclude.psm1 -Destination $OutFile -Verbose
             } -ParameterFilter { $Uri -eq "https://github.com/BrightEdgeeServices/venvit/releases/download/$MockTag/Install-Conclude.psm1" }
+            Write-Host "*** Checkpoint 4 ***"
             Mock Import-Module {
                 Import-Module "$PSScriptRoot\..\src\Install-Conclude.psm1" -Verbose
             } -ParameterFilter { $Name.StartsWith($env:TEMP) }
             # Mock -ModuleName Install-Conclude -CommandName Invoke-ConcludeInstall {
+            Write-Host "*** Checkpoint 5 ***"
             Mock Invoke-ConcludeInstall {
                 "exit" | Out-File -FilePath "$env:VENVIT_DIR\vn.ps1" -Force
                 "exit" | Out-File -FilePath "$env:VENVIT_DIR\vi.ps1" -Force
@@ -38,8 +43,10 @@
                 "exit" | Out-File -FilePath "$env:VENV_SECRETS_DIR\dev_env_var.ps1" -Force
             }
 
+            Write-Host "*** Checkpoint 6 ***"
             Invoke-Install
 
+            Write-Host "*** Checkpoint 7 ***"
             Assert-MockCalled -CommandName "Invoke-WebRequest" -Exactly 2
             Assert-MockCalled -CommandName "Invoke-ConcludeInstall" -Exactly 1
         }
